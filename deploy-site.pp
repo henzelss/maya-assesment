2a5979dff899138b56b6f9258dec82be46890395
henzelprosperna <henzel@prosperna.com>
2025-01-25 22:13:02 +0800

    Added user monitor

--- deploy-site.pp
+++ deploy-site.pp
@@ -4,3 +4,26 @@
     ensure => installed,
 
  }
+ 
+# Create user "monitor"
+user { 'monitor':
+  ensure     => present,
+  home       => '/home/monitor',
+  shell      => '/bin/bash',
+  managehome => true,
+}
+
+# Create required directories
+file { '/home/monitor/scripts':
+  ensure => directory,
+  owner  => 'monitor',
+  group  => 'monitor',
+  mode   => '0755',
+}
+
+file { '/home/monitor/src':
+  ensure => directory,
+  owner  => 'monitor',
+  group  => 'monitor',
+  mode   => '0755',
+}
