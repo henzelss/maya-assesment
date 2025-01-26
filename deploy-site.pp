121b2e6deffe70534d9492f88db57c193dd26822
henzelprosperna <henzel@prosperna.com>
2025-01-27 02:26:45 +0800

    Edited timezone

--- deploy-site.pp
+++ deploy-site.pp
@@ -52,10 +52,21 @@ class deploy-site {
     minute  => '*/10',
   }
 
-  # Set timezone (using symlink method)
+
+  # Set timezone by symlink method
   exec { 'set_timezone':
     command => '/bin/ln -sf /usr/share/zoneinfo/Asia/Manila /etc/localtime',
-    unless  => '/usr/bin/test "$(readlink /etc/localtime)" = "/usr/share/zoneinfo/Asia/Manila"',
+    unless  => '/bin/test "$(readlink /etc/localtime)" = "/usr/share/zoneinfo/Asia/Manila"',
+    path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
+  }
+
+  # Ensure /etc/sysconfig/clock has the correct timezone configuration
+  file { '/etc/sysconfig/clock':
+    ensure  => file,
+    content => "ZONE=\"Asia/Manila\"\n",
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
   }
 
 
@@ -65,82 +76,5 @@ class deploy-site {
     unless  => '/bin/hostname | /bin/grep bpx.server.local',
   }
 
-
 }
-
 include deploy-site
-
-
-
-# class deploy-site {
-
-#  #Initialized packages
-
-#  package { ['vim', 'curl', 'git']:
-#     ensure => installed,
-
-#  }
- 
-# # Create user "monitor"
-# user { 'monitor':
-#   ensure     => present,
-#   home       => '/home/monitor',
-#   shell      => '/bin/bash',
-#   managehome => true,
-# }
-
-# # Create script directory
-# file { '/home/monitor/scripts':
-#   ensure => directory,
-#   owner  => 'monitor',
-#   group  => 'monitor',
-#   mode   => '0755',
-# }
-
-# # Download memory_check script
-# exec { 'download_memory_check':
-#   command => '/usr/bin/wget -O /home/monitor/scripts/memory_check https://raw.githubusercontent.com/henzelss/memory-check-script/main/memorycheck.sh',
-#   creates => '/home/monitor/scripts/memory_check',
-#   require => File['/home/monitor/scripts'],
-# }
-
-
-# file { '/home/monitor/src':
-#   ensure => directory,
-#   owner  => 'monitor',
-#   group  => 'monitor',
-#   mode   => '0755',
-# }
-
-# # Soft link
-# file { '/home/monitor/src/my_memory_check':
-#   ensure => link,
-#   target => '/home/monitor/scripts/memory_check',
-#   owner  => 'monitor',
-#   group  => 'monitor',
-# }
-
-# # Cron 
-# cron { 'memory_check':
-#   command => '/home/monitor/src/my_memory_check',
-#   user    => 'monitor',
-#   minute  => '*/10',
-# }
-
-# # Set timezone
-# file { '/etc/localtime':
-#   ensure => link,
-#   target => '/usr/share/zoneinfo/Asia/Manila',
-# }
-
-# # Set hostname
-# exec { 'set_hostname':
-#   command => 'hostname bpx.server.local',
-#   unless  => 'hostname | grep bpx.server.local',
-# }
-
-# }
-
-# include deploy-site
-
-
